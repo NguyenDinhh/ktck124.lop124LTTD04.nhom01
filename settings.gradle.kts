aa00f36c5508643c5e0ec182a22d3120e4d78722
NguyenDinhh <dinhhandsome12a1@gmail.com>
2024-12-21 10:19:59 +0700

    Khởi tạo project

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ktck124.lop124LTTD04.nhom01"
+include(":app")
+ 
